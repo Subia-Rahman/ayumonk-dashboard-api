4ac821cc8b757879df2f0e6eb4a12822b68bed6a
Subia Rahman <subia.rahman@wiredsoft.org>
2026-05-20 16:16:31 +0530

    Temporary change to make the temporary https url work

--- gateway/main.py
+++ gateway/main.py
@@ -34,8 +34,9 @@ app = FastAPI(title="API Gateway", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*","https://grades-assets-civilization-optional.trycloudflare.com"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    #allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
